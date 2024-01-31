2fb84c8dc4eb2cee3d2009e4c902275ab3edb3f0
ngviet00 <ngvviet0306@gmail.com>
2024-01-31 11:19:24 +0700

    init project

---
+++ RevenueManagement/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RevenueManagement.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
